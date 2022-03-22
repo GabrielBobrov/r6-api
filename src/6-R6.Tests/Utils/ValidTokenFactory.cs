e1e998a30603471aac8502c364c5beb1e687a034
Gabriel Bobrov <bielbobrov@hotmail.com>
2022-03-21 23:11:08 -0300

    Add jwt

---
+++ src/6-R6.Tests/Utils/ValidTokenFactory.cs
@@ -0,0 +1,14 @@
+namespace R6.Tests.Utils
+{
+    public static class ValidTokenFactory
+    {
+        public static string ValidToken()
+        {
+            return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiamF2YXJ1aW0iLCJyb2xlIjoiVXNlciIsIm5iZiI6MTY0Nzg4NDQ2MiwiZXhwIjo1MjQ3ODg0NDYyLCJpYXQiOjE2NDc4ODQ0NjJ9.DIt40jnCZ7G7XxeKGMXr8oPSmO0QDlR5waRnStoDp-0";
+
+        }
+    }
+}
+
+
+
